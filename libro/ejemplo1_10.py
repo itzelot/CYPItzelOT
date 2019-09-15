d413691416187e0f4abe233e4908b537a1ff4f83
itzelot <itzelot01@gmail.com>
2019-09-15 14:10:58 -0500

    Problema 1_10 del libro

---
+++ libro/ejemplo1_10.py
@@ -0,0 +1,6 @@
+BASE = float(input("Ingresa la longitud de Base"))
+ALTU = float(input("Ingresa la longitud de Altura"))
+SUP = BASE * ALTU
+PER = 2 * (BASE + ALTU)
+print(f"La superficie del rectangulo es { SUP }")
+print(f"El perimetro del rectangulo es { PER }")
