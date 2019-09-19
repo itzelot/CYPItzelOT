f597142e8c1866e5cd23c15cf0402229fa68d8bf
itzelot <itzelot01@gmail.com>
2019-09-18 21:59:57 -0500

    Problema 1_13 del libro

---
+++ libro/ejemplo1_13.py
@@ -0,0 +1,7 @@
+CAL1 = float(input("Ingresa la calificación 1:"))
+CAL2 = float(input("Ingresa la calificación 2:"))
+CAL3 = float(input("Ingresa la calificación 3:"))
+CAL4 = float(input("Ingresa la calificación 4:"))
+CAL5 = float(input("Ingresa la calificación 5:"))
+PRO = (CAL1 + CAL2 + CAL3 + CAL4 + CAL5) /5
+print(f"El promedio es { PRO }") 
