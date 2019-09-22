1953d0d8dac8e6280a14c9484072a68dc0dbe835
itzelot <itzelot01@gmail.com>
2019-09-22 16:15:23 -0500

    Problema 1_15 del libro

---
+++ libro/ejemplo1_15.py
@@ -0,0 +1,5 @@
+BASE = float(input("Ingresa la longitud de la base del rectangulo:"))
+ALTU = float(input("Ingresa la longitud de la altura del rectangulo:"))
+SUP = BASE * ALTU
+PER = 2*(BASE + ALTU)
+print(f"El área del rectangulo ingresado es {SUP} y el perímetro del rectangulo es {PER}")
