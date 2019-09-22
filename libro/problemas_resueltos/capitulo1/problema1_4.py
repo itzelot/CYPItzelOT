ea3bf661ef44927d444d281f4db86c6dc278f1d2
itzelot <itzelot01@gmail.com>
2019-09-22 16:30:34 -0500

    Se agregó el problema 1_4 del libro

--- libro/problemas_resueltos/capitulo1/problema1_4.py
+++ libro/problemas_resueltos/capitulo1/problema1_4.py
@@ -1,1 +1,3 @@
-GAL = float(input("
+GAL = float(input("Ingresa el número de galones de Gasolina que se le surtieron al cliente:")) 
+TOTAL = GAL * 3.785 * 8.20
+print(f"El precio a pagar por los galones de gasolina en GALONES: {GAL} comprados es igual a {TOTAL}")
