8a915da50236b7722f8d9c2baa06dec9b2f794b1
itzelot <itzelot01@gmail.com>
2019-10-02 11:56:17 -0500

    Problema 2_3 del libro

---
+++ libro/ejemplo2_3.py
@@ -0,0 +1,7 @@
+CAL = float(input("Ingresa tu calificacion:"))
+if CAL >= 8:
+    print(f"Tu calificacion es de: {CAL}")
+    print("Aprobado")
+else:
+    print("Reprobado")
+print("Fin del programa")
