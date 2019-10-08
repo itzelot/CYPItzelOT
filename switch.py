4a466d74c8cacc800dec92a2109723c6f16d8ed5
itzelot <itzelot01@gmail.com>
2019-10-08 16:45:29 -0500

    Codigo switch

---
+++ switch.py
@@ -0,0 +1,16 @@
+dia = int(input("Introduce un día (1 - 5):"))
+if dia == 1:
+    print("Lunes")
+elif dia == 2:
+    print("Martes")
+elif dia == 3:
+    print("Miercoles")
+elif dia == 4:
+    print("Jueves")
+elif dia == 5:
+    print("Viernes")
+else:
+    print("Opcion no valida")
+
+
+print("Fin del programa!!")
