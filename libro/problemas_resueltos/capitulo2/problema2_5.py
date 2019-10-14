91821ac3aeae00974f90034a5be039edff50493d
itzelot <itzelot01@gmail.com>
2019-10-13 20:32:34 -0500

    Se agregó el problema 2_5 del libro

---
+++ libro/problemas_resueltos/capitulo2/problema2_5.py
@@ -0,0 +1,10 @@
+NUM = int(input("Ingresa in número entero:"))
+if NUM > 0:
+       print(f"{NUM} es un número positivo")
+else:
+       if NUM = 0:
+              print(f"{NUM} es nulo")
+       else:
+              print(f"{NUM} es un número negativo")
+
+print("Fin del programa")
