3a68bbef9095a34276a1ac3a5f33e3f1ba54dd34
itzelot <itzelot01@gmail.com>
2019-10-13 20:38:43 -0500

    Se agregó problema 2_6 del libro

---
+++ libro/problemas_resueltos/capitulo2/problema2_6.py
@@ -0,0 +1,11 @@
+A = int(input("Ingresa in número entero"))
+
+if A = 0:
+    print(f"{A} es nulo")
+else:
+    if -1 ** A > 0:
+        print(f"{A} es par")
+    else:
+        print(f"{A} es impar")
+
+print("Fin del programa")
