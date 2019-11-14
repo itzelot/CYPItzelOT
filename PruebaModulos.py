0a959ddd7ff811708b9556bfa219934a3bcb2ff6
itzelot <itzelot01@gmail.com>
2019-11-14 14:31:45 -0600

    Clase 12 de noviembre

---
+++ PruebaModulos.py
@@ -0,0 +1,39 @@
+#import Modulos
+#Modulos.mi_print("Hola")
+
+from Modulos import *
+import time
+import sys
+from asciistuff import Banner
+
+mi_print("Hola de nuevo")
+otro_print("otro print usado")
+print(sumar(4, 5))
+print(restar(10 , 7))
+
+for i in range(10,0,-1):
+    print(i, "...")
+    time.sleep(0)#pausa el tiempo en segundos
+print(Banner("BOOOM!!!"))
+#print(sys.platform)
+#import Modulos
+#Modulos.mi_print("Hola")
+
+from Modulos import *
+import time
+import sys
+from asciistuff import Banner
+
+mi_print("Hola de nuevo")
+otro_print("otro print usado")
+print(sumar(4, 5))
+print(restar(10 , 7))
+
+for i in range(10,0,-1):
+        print(i, "...")
+            time.sleep(0)#pausa el tiempo en segundos
+            print(Banner("BOOOM!!!"))
+            #print(sys.platform)
+
+            print(Banner("ICO FES ARAGON"))
+print(Banner("ICO FES ARAGON"))
