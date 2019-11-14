3f60ea6e69cac761b39663b1de73a819aed671f5
itzelot <itzelot@gmail.com>
2019-11-14 17:19:19 -0600

    Clase 14 de noviembre

---
+++ cell_db.py
@@ -0,0 +1,35 @@
+# cell_db.py
+
+def leer_datos( path ):
+    file = open(path, 'rt')
+    lista_final=[]
+    dic_cel={}
+    datos = file.readlines()
+    #print(datos)
+    for ren in range(len(datos)):
+        datos[ren]= datos[ren].strip().split(',')
+    #print(datos)
+    for ren in range(1 , len(datos), 1):
+        dic_cel={}
+        for col in range(len(datos[ren])):
+            dic_cel[datos[0][col].strip()]= datos[ren][col]
+        lista_final.append(dic_cel)
+    #print(lista_final)
+    return lista_final
+
+def salida_formato(celular):
+    print(f"Celular marca: {celular['marca']}")
+    print(f" \t Modelos celular: {celular['modelo']}")
+    print(f" \t Almacenamiento: {celular['almacenamiento']}")
+    print(f" \t Velocidad: {celular['velocidad']}")
+    print(f" \t Memoria interna: {celular['ram']}")
+    print(f" \t Color: {celular['color']}")
+    
+
+def main():
+    archivo = "celulares.txt"
+    bd = leer_datos(archivo)
+    print(f"DB = {bd}")
+    salida_formato(bd[6])
+
+main()
